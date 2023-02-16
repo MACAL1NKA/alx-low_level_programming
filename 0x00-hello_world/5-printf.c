ce597c5a5030200f88abfabb5f0f845e7834b561
Abdillahi Abdinasir Omer <mr.jasta9@gmail.com>
2023-02-16 12:46:17 -0800

    hello

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+/*
+ * File: 5-printf.c
+ * print strun 
+ */
+
+#inclue <stdio.h>
+
+/**
+ *main - Prints a value
+ *                of string
+ *
+ *Return: Always 0.
+?
+*/
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,");
+
+  return (0);
+}
