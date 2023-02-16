c5f53868e63c74c0386b4c43ff7c3dd40385e4c3
Abdillahi Abdinasir Omer <93239543+MACAL1NKA@users.noreply.github.com>
2023-02-16 23:37:12 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,20 +10,10 @@
  *            and flaue
  *
  *great work
-<<<<<<< HEAD
  */
-int main()
-=======
- */ 
 int main(void)
->>>>>>> 5d5b13f251e0cb54bbab09a1ff4d49890433cee0
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-<<<<<<< HEAD
-	
 	return (0);
-=======
-	return 0;
->>>>>>> 5d5b13f251e0cb54bbab09a1ff4d49890433cee0
 }
