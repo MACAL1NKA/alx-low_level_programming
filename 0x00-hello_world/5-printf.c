45430a02f75d7fea52d554841cd89d534e302ced
Abdillahi Abdinasir Omer <93239543+MACAL1NKA@users.noreply.github.com>
2023-02-16 23:49:17 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -10,11 +10,10 @@
  *                of string
  *
  *Return: Always 0.
-?
 */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,");
+  printf("with proper grammar, but the outcome is a piece of art,\n");
 
   return (0);
 }
