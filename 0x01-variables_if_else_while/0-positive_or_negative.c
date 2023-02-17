fad0ab35a1c8f5cdebeeddb80fd38627a281cadb
Abdillahi Abdinasir Omer <93239543+MACAL1NKA@users.noreply.github.com>
2023-02-17 21:38:52 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
+/**
  *main - prints is the neber is positive or not
- *
+ *Description: this an a random program
  *Return: Alaways 0.
  */
 int main(void)
