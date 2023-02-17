b5426d0c044d7740ab495459bc0edc692cba54e6
Abdillahi Abdinasir Omer <93239543+MACAL1NKA@users.noreply.github.com>
2023-02-17 23:04:17 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,20 +7,14 @@
  */
 int main(void)
 {
-	
-int number;
-char letter;
+	int number;
+	char letter;
 
-	
 	for (number = 0; number < 10; number++)
-	{
-	    putchar((number % 10) + '0');
-	}
+		putchar((number % 10) + '0');
 
 	for (letter = 'a'; letter <= 'f'; letter++)
-	{
-	    putchar(letter);
-	}
+		putchar(letter);
 
 	putchar('\n');
 
