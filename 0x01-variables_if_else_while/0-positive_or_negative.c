68e54a5d7a8cf79e4bc56884534d79361a56fd28
Abdillahi Abdinasir Omer <mr.jasta9@gmail.com>
2023-02-17 10:13:02 -0800

    00 update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-
+#include <stdio.h>
 /*
  *main - prints is the neber is positive or not 
  *
@@ -14,17 +14,17 @@ int main(void)
   n = rand() - RAND_MAX /2;
   if (n>0)
     {
-      printf("%d is possitive",n);
+      printf("%d is possitive\n",n);
     }
   else
     {
-      if (n,0)
+      if (n<0)
 	{
-	  printf("%d is negative",n);
+	  printf("%d is negative\n",n);
 	}
       else
 	{
-	  printf("%d is zero",n);
+	  printf("%d is zero\n",n);
 	}
     }
   return (0);
