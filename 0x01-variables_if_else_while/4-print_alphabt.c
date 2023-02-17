4e10f96f481ee08b08993c6eb07420bcb700d73c
Abdillahi Abdinasir Omer <93239543+MACAL1NKA@users.noreply.github.com>
2023-02-17 23:12:39 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,11 +12,9 @@ char letter;
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 	    if (letter != 'q' && letter != 'e')
-	    {
-	        putchar(letter);
-	    }
+		    putchar(letter);
 	}
-	
+
 	putchar('\n');
 
 	return (0);
