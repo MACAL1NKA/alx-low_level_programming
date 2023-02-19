eff715c5d9101576c4441141f4b39c8311cac7bb
Abdillahi Abdinasir Omer <mr.jasta9@gmail.com>
2023-02-18 20:53:33 -0800

    last advansed project

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
  *main - Prints the quots 
  *                  and numbers
  *
- *Return = Always 1.
+ *Return:  Always 1.
  */
 int main() {
 fwrite(2,"and that piece of art is useful\" - Dora Korpar, 2015-1 -19\n",59);
