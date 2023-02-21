3c95daae2114bdbfd7c6f6057389fb4e129e1754
Abdillahi Abdinasir Omer <mr.jasta9@gmail.com>
2023-02-21 03:45:27 -0800

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints one line 
+ *
+ * Return: Always 0.
+ * /
+main(void)
+{
+printf("_putchar\n");
+
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+fuction and nested loops
