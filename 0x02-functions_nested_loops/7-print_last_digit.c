5bb7bf2cdb91a39da37de16660ce34cd94849ec8
Abdillahi Abdinasir Omer <93239543+MACAL1NKA@users.noreply.github.com>
2023-02-21 22:52:42 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - Prints the last digit.
- *
+ *@n: is a number
  * Return: last degit.
  */
 int print_last_digit(int n)
@@ -11,7 +11,6 @@ int print_last_digit(int n)
 
 	if (last_digit < 0)
 		last_digit *= -1;
-  
 
 	_putchar(last_digit + '0');
 
