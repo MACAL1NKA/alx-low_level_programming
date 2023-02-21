3ad9ac033b43fa146d1b9dd7952e58e979af7032
Abdillahi Abdinasir Omer <mr.jasta9@gmail.com>
2023-02-21 10:22:31 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * islower.c - Print 1 if lower case eles 0.
+ *
+ * Return: 1 if true, elso 0.
+ */
+int _islower(int c)
+	{
+		if (c > 'a' && c < 'z)
+			return (1);
+		else
+			return (0);
+	}
