33b70dce8e22ce03c40eba44781e05c643d0418e
Abdillahi Abdinasir Omer <mr.jasta9@gmail.com>
2023-02-21 10:01:09 -0800

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - print alphaber 10 time.
+ */
+void print_alphabet_x10(void)
+{
+char letter;
+int cont = 0;
+
+while(cont<10)
+{
+for (letter = 'a'; letter<= 'z'; letter++)
+	putchar(letter);
+putchar('\n');
+cont++;
+}
+}
