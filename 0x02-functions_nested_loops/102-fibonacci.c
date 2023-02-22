c235ed8187e2113bcf496928ad0803613bcb6371
Abdillahi Abdinasir Omer <mr.jasta9@gmail.com>
2023-02-22 11:13:58 -0800

    all the files done

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints sums.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int c;
+	unsigned long f1 = 0, f2 = 1, sum;
+
+	for (c = 0; c < 50; c++)
+	{
+		sum = f1 + f2;
+		printf("%lu", sum);
+
+		f1 = f2;
+		f2 = sum;
+
+		if (c == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
