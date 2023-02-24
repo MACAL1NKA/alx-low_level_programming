38b9cd9694d0991725739e9e8d14ef0573c85dc0
Abdillahi Abdinasir Omer <mr.jasta9@gmail.com>
2023-02-24 03:47:51 -0800

    new apps

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+include "main.h"
+
+/**
+ * _isupper - Check if a letter is upper
+ * @x: The numberd
+ * Return: 1 or 0 
+ */
+
+int _isupper(int x)
+
+{
+
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
+
