c08c06520d292c824e83d4b6773a3d3634770dde
Abdillahi Abdinasir Omer <mr.jasta9@gmail.com>
2023-02-27 12:06:01 -0800

    more

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *puts2 - print odd numbers
+ *@str: imput string
+ *
+ *Return: no
+ */
+void puts2(char *str)
+{
+    int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+
+	    j++;
+
+	}
+
+	for (i = 0; i < j; i += 2)
+	{
+
+	    _putchar(str[i]);
+
+	}
+
+	    _putchar('\n');
+}
