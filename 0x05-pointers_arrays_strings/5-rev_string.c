36e5ad4faf603fb9be47fb649305894160ffbfd4
Abdillahi Abdinasir Omer <mr.jasta9@gmail.com>
2023-02-27 11:33:58 -0800

    more updlop

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,9 +16,8 @@ void rev_string(char *s)
 	    length++;
 
 	for (index = length - 1; index >= length / 2; index--)
-	{
-	tmp = s[index];
-	s[index] = s[length - index - 1];
-	s[length - index - 1] = tmp;
-	}
+		tmp = s[index];
+		s[index] = s[length - index - 1];
+		s[length - index - 1] = tmp;
+	
 }
