e206351d463fda6eb8d5686640e2b7111b918bed
Abdillahi Abdinasir Omer <mr.jasta9@gmail.com>
2023-02-27 11:45:31 -0800

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,8 +16,9 @@ void rev_string(char *s)
 	    length++;
 
 	for (index = length - 1; index >= length / 2; index--)
+	{
 		tmp = s[index];
 		s[index] = s[length - index - 1];
 		s[length - index - 1] = tmp;
-	
+	}
 }
