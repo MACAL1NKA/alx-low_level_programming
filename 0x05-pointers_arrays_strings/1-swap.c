4b3faf8f8f3dea58e9ea63e6688b5a61358410fe
Abdillahi Abdinasir Omer <mr.jasta9@gmail.com>
2023-02-27 10:18:30 -0800

    swap project

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *void swap_int - swaps tow values with difrent varfiable.
+ * @a: first inbut 
+ * @b: second input
+ * Return: no
+ */
+void swap_int(int *a, int *b)
+{
+	int z = *a;
+	*a = *b;
+	*b = z;
+}
