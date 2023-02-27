478d8fa0c78af707d0e9595206972a3fae9f90ce
Abdillahi Abdinasir Omer <mr.jasta9@gmail.com>
2023-02-27 12:11:38 -0800

    put

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,18 +13,10 @@ void puts2(char *str)
 	int j = 0;
 
 	while (str[j] != '\0')
-	{
-
 	    j++;
 
-	}
-
 	for (i = 0; i < j; i += 2)
-	{
-
 	    _putchar(str[i]);
 
-	}
-
-	    _putchar('\n');
+	_putchar('\n');
 }
