616d933b0652ea05a4811509f81e08b9fefbe18b
Abdillahi Abdinasir Omer <mr.jasta9@gmail.com>
2023-02-28 04:15:51 -0800

    9 project uploaded

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - print string of pinter
+ * @dest: input 
+ * @src: input
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+for (; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+dest[i++] = '\0';
+return (dest);
+}
