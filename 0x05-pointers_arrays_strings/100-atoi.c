7a95a85e2ffd261860f61e678738fe448ad2e829
Abdillahi Abdinasir Omer <mr.jasta9@gmail.com>
2023-02-28 11:33:01 -0800

    public

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - print more 
+ * @s: input string
+ * Rerurn: i value
+ */
+int _atoi(char *s)
+
+{
+
+	int c = 0;
+	unsigned int i = 0;
+	int j = 1;
+	int k = 0;
+
+	while (s[c])
+	{
+	if (s[c] == 45)
+	{
+	j *= -1;
+	}
+	while (s[c] >= 48 && s[c] <= 57)
+	{
+	k = 1;
+	i = (i * 10) + (s[c] - '0');
+	c++;
+	}
+	if (k == 1)
+	{
+	break;
+	}
+	c++;
+	}
+	i *= j;
+	return (i);
+}
