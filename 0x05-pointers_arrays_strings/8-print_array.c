143ba378165cce8a18ca226fac0460d8afc80de7
Abdillahi Abdinasir Omer <mr.jasta9@gmail.com>
2023-02-28 04:00:39 -0800

    8-print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print the array
+ * @a: input one
+ * @n: input 2
+ */
+void print_array(int *a, int n)
+
+{
+
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+	printf("%d", a[j]);
+	if (j < n - 1)
+	{
+	printf(", ");
+	}
+	}
+	printf("\n");
+}
