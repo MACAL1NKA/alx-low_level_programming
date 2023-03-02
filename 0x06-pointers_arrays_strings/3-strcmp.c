3f0ed770baf4613af29a0ba52e529281fd6e5ee2
Abdillahi Abdinasir Omer <mr.jasta9@gmail.com>
2023-03-02 08:12:10 -0800

    hard example

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strcmp - siffrence between two strings
+ *@s1: first input
+ *@s2: second input
+ *Return: output
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0, b = 0, c = 0, r = 0, l;
+
+	while (s1[a])
+	{
+		a++;
+	}
+    printf("a =%d b=%d c=%d\n",a,b,c);
+	while (s2[b])
+	{
+		b++;
+	}
+    printf("a =%d b=%d c=%d\n",a,b,c);
+	if (a <= b)
+	{
+		l = a;
+	}
+	else
+	{
+		l = b;
+	}
+
+	while (c <= l)
+	{
+		if (s1[c] == s2[c])
+		{
+			c++;
+			continue;
+		}
+		else
+		{
+			r = s1[c] - s2[c];
+			break;
+		}
+
+		c++;
+	}
+    printf("a =%d b=%d c=%d\n",a,b,c);
+	return (r);
+}
