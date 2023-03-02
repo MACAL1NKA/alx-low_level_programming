e54a671396b605fd895cf2107697fe43ef81cfd7
Abdillahi Abdinasir Omer <mr.jasta9@gmail.com>
2023-03-01 21:51:01 -0800

    new project

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strcat - addss two str
+ *@dest: input 1
+ *@src: input 2
+ *Return: @dest
+ */
+char *_strcat(char *dest, char *src){
+     int length = 0;
+     int j = 0;
+
+    while (dest[length] != '\0')
+    {
+     ++length;
+    }
+
+  for (; src[j] != '\0'; ++j, ++length) {
+    dest[length] = src[j];
+  }
+  dest[length] = '\0';
+  return dest;
+ }
