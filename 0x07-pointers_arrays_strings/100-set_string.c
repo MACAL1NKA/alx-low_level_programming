7aef794a4c60ee486e4a4b7e479d9c8aea24da36
Abdillahi Abdinasir Omer <mr.jasta9@gmail.com>
2023-03-06 11:39:51 -0800

    ff

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_string - Prints diffrent sums 
+ * @s: input string 
+ *@to: is the size of array
+ * Return: nothing
+ */
+ 
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
