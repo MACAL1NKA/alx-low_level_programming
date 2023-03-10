9df55bdb7fcd1c48d1ba80c67da517abbf9e4cca
Abdillahi Abdinasir Omer <mr.jasta9@gmail.com>
2023-03-10 08:39:19 -0800

    new project

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - adds infinatie numbers
+ * @argc: number of arrays
+ * @argv: input arrays
+ * Return: 0 or one or Error
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	long unsigned int j, sum;
+	char *array;
+
+	if (argc > 1)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			array = argv[i];
+
+			for (j = 0; j < strlen(array); j++)
+			{
+				if (array[j] < 48 || array[j] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(array);
+			array++;
+		}
+		printf("%ld\n", sum);
+	}
+	else
+	{
+	
+	printf("0\n");
+	}
+	return (0);
+
+}
