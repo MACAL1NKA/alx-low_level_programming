d374658ea367b0590900ca08329744b90c4849d7
Abdillahi Abdinasir Omer <mr.jasta9@gmail.com>
2023-03-20 11:48:24 -0700

    new apudar

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+#include <stdio.h>
 /**
  * print_dog - this print dog infor
  *
@@ -14,6 +14,6 @@ void print_dog(struct dog *d)
 		d->name == NULL ? printf("(nil)\n") : printf("%s\n", d->name);
 		printf("Age: %f\n", d->age);
 		printf("Owner: ");
-		d-.owner == NULL ? printf("(nil)\n") : printf("%s\n", d->owner;
+		d->owner == NULL ? printf("(nil)\n") : printf("%s\n", d->owner);
 	}
 }
