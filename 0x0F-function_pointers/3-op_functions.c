e1c5d50aaecc86e502a95bc2720e6d7346c37bc4
Abdillahi Abdinasir Omer <mr.jasta9@gmail.com>
2023-03-24 19:43:25 -0700

    new update

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+/**
+ * op_add - adds a and b
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: addition
+*/
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+
+/**
+ * op_sub - difrence of a and b
+ *
+ * @a: input 
+ * @b: input 
+ *
+ * Return: difference a and b
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+
+/**
+ * op_mul - multiplies a and p
+ * @a: input 
+ * @b: input 
+ *
+ * Return: product of two
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+
+/**
+ * op_div - divides tow values
+ * @a: input 
+ * @b: input 
+ * Return: divition
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - gets the remidnedr
+ * @a: input 1
+ * @b: input 2
+ * Return: the remainder of the two inputs
+*/
+int op_mod(int a, int b)
+{
+return (a % b);
+}
